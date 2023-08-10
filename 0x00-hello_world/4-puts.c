9923e249c6ea881ce4c45f421f755b500a0fd0dc
faithFey <faithnasimiyuu@gmail.com>
2023-08-10 13:49:54 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main-  Hello world project.
+ *Return:returns 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
