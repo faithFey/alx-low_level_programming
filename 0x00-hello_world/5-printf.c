8f3c087ee1f87904986488290036e978931e772e
faithFey <faithnasimiyuu@gmail.com>
2023-08-10 15:09:10 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ *main- use printf to print out.
+ *
+ *Return:returns zero on success.
+ */
+int main(void)
+{
+printf("with proper grammar\, but the outcome is a piece of art\,");
+return (0);
+}
+
