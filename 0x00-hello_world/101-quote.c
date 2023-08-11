fb7905a74a7cf2e41ea3a5cb6f48ca4db200ca25
faithFey <faithnasimiyuu@gmail.com>
2023-08-10 17:21:31 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<unistd.h>
+
+
+/**
+ *main- print without printf or puts
+ *
+ *Return:returns a value of 1
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
