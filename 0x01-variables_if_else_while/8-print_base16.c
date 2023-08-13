9d18c9981af8839d28c7cad63d25ceb690d627a6
faithFey <faithnasimiyuu@gmail.com>
2023-08-13 06:40:03 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ *main -program that prints all the numbers of base 16 in lowercase
+ *Return: return 0 on success.
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
