682a260fbc9aff63ed065225b6328204d785a4a5
faithFey <faithnasimiyuu@gmail.com>
2023-08-16 01:37:42 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - function that prints the alphabet, in lowercase, followed by a new line
+ *Return: return 0 on success.
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= '2'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
