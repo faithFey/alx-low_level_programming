380c3cf597468b4465a01ff6c33853ee3a655eaa
faithFey <faithnasimiyuu@gmail.com>
2023-08-16 03:34:22 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Make alphabet *10 times
+ *Return:0 on success.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+	i=0;
+
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+			_putchar('\n');
+			i++;
+		}
+}
