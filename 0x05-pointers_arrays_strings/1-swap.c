5e6573900f6b61c3ee61fa7926f6cc214ca752d9
faithFey <faithnasimiyuu@gmail.com>
2023-08-21 11:57:15 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int - exchange the value of a and b.
+ *@a:The first value.
+ *@b:The second value.
+ *Return: returns nothing.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
