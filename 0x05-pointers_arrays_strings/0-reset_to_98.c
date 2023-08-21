098636997901c12a459a7061773fa4741b3366dd
faithFey <faithnasimiyuu@gmail.com>
2023-08-21 11:32:47 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 -reset the value of n to 98
+ *@n:The number to be tested.
+ *Return:nothing to return
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,10 @@
+#include<unistd.h>
+
+/**
+ *_putchar - puts character
+ *Return:return 0 on success.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
