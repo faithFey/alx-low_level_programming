87eea6d6a6000306f4497310bbab0e9f7e407f56
faithFey <faithnasimiyuu@gmail.com>
2023-09-11 14:00:56 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#ifndef _FILE_
-#define _FILE_
-#endif
+
 /**
  *main - print the file name
  *Return:return 0 on success.
  */
+
 int main(void)
 {
-	printf("%s\n",_FILE_);
+	printf("%s\n", _FILE_);
 	return (0);
 }
