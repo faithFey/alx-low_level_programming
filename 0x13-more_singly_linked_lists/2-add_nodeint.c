31cef5e9ac9f8b47abea7c9cad1c1f8551ee3838
faithFey <faithnasimiyuu.com>
2023-10-24 14:01:56 -0700

    task 2 f

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,11 +9,9 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *firstNode;
+	listint_t *firstNode =  malloc(sizeof(listint_t));
 
-	firstNode =  malloc(sizeof(listint_t));
-
-	if (!firstNode)
+	if (firstNode == NULL)
 	return (NULL);
 	firstNode->n = n;
 	firstNode->next = *head;
