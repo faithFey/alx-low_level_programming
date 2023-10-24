df1ffbd4f037de057ca2d42b71f3ef73d5d4848c
faithFey <faithnasimiyuu.com>
2023-10-24 14:42:11 -0700

    free linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**free_listint - frees the listint linked list
+ *@head:pointer to the head of the node.
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
