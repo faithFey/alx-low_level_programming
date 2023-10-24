6b0ad260360707d028e8ebd9af093820e7dfbc53
faithFey <faithnasimiyuu.com>
2023-10-24 14:33:05 -0700

    adding the starting node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,10 +9,18 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *firstNode =  malloc(sizeof(listint_t));
+	listint_t *firstNode;
 
+	firstNode = malloc(sizeof(listint_t));
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	if (firstNode == NULL)
-	return (NULL);
+	{
+		return (NULL);
+	}
 	firstNode->n = n;
 	firstNode->next = *head;
 	*head = firstNode;
